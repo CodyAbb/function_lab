8e58f4b42d6efe96974547fd55a446148cc5d125
Cody Abbott <cody.abbott16@gmail.com>
2019-11-19 16:07:44 +0000

    with date modifications

--- ruby_functions.rb
+++ ruby_functions.rb
@@ -1,3 +1,5 @@
+require 'date'
+
 def return_10
   return 10
 end
@@ -31,33 +33,12 @@ def add_string_as_number(string_1, string_2)
 end
 
 def number_to_full_month_name(month_num)
-  case month_num
-  when 1
-    return "January"
-  when 2
-    return "Februrary"
-  when 3
-    return "March"
-  when 4
-    return "April"
-  when 9
-    return "September"
-  else
-    return "error"
-  end
+  return Date::MONTHNAMES[month_num]
 end
 
 def number_to_short_month_name(month_num)
-  case month_num
-  when 1
-    return "Jan"
-  when 4
-    return "Apr"
-  when 10
-    return "Oct"
-  else
-    "error"
-  end
+  result = number_to_full_month_name(month_num)
+  return result[0,3]
 end
 
 def volume_of_cube(x)
